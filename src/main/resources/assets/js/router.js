32e47438e5932dc5c1a6985d41e628345ac4cab3
Karl Leswing <lilleswing@gmail.com>
2014-06-15 13:03:06 -0400

    remove footer from the FE

--- src/main/resources/assets/js/router.js
+++ src/main/resources/assets/js/router.js
@@ -46,12 +46,6 @@ define([
         homeView.render();
     });
 
-    // Unlike the above, we don't call render on this view as it will handle
-    // the render call internally after it loads data. Further more we load it
-    // outside of an on-route function to have it loaded no matter which page is
-    // loaded initially.
-    var footerView = new FooterView();
-
     Backbone.history.start();
   };
   return { 
