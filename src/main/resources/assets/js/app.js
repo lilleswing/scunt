5f9b4dc91346d305d3b4f5884fc4e340d569ee7b
Karl Leswing <lilleswing@gmail.com>
2014-06-28 17:27:03 -0400

    Google + signin without call back

--- src/main/resources/assets/js/app.js
+++ src/main/resources/assets/js/app.js
@@ -1,15 +1,19 @@
 ( function() {
-    var app = angular.module('scunt', []);
+    var app = angular.module('scunt', ['directive.g+signin']);
     app.controller("ScuntController", function() {
         this.settings = {
-            tab: "home"
+            tab: "home",
+            loggedIn: false
         };
         this.setTab = function(newTab) {
             this.settings.tab = newTab;
-        }
+        };
         this.isTab = function(newTab) {
             return this.settings.tab === newTab;
-        }
+        };
+        this.isLoggedIn = function() {
+            return this.settings.loggedIn;
+        };
     });
 
     app.directive("menuNavbar", function() {
